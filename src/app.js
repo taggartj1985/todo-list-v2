25ebc185399625a61799a3edd25800da8434a044
John Taggart <taggartj1985@gmail.com>
2020-04-20 17:25:28 +0100

    got save feature working can't see item list tho

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
+      todos: ["Walk Dog", "Washing", "Play Games"],
 
+      newTodo: ""
     },
     methods: {
+      saveNewTodo: function(){
+        this.todos.push(this.newTodo);
+        this.newTodo = "";
+      }
 
     }
   });
